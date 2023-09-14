a8e94522dc584b6814715ae8ef3b52f2a1106cf5
Swaroop <141646466+Swaroop5222@users.noreply.github.com>
2023-09-14 18:42:42 +0530

    Add files via upload

---
+++ check whether the number is prime or not.py
@@ -0,0 +1,6 @@
+num=int(input("Enter the number\n"))
+
+if(num==2 or num%num==0 and num%1==0 and num%2!=0):
+    print("The number is a prime number\n")
+else:
+    print("The number is a composite number\n")
