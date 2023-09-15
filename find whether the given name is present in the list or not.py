cf55c2738e04254ede6808c6889e2bbbf2137b2f
Swaroop <141646466+Swaroop5222@users.noreply.github.com>
2023-09-15 18:44:16 +0530

    Add files via upload

---
+++ find whether the given name is present in the list or not.py
@@ -0,0 +1,8 @@
+name=input("Enter the name: ")
+
+Listofnames=['Swaroop','Teja','Sts','Pavansai']
+
+if(name in Listofnames):
+    print("The name you entered is present")
+else:
+    print("The name you entered is not present")
