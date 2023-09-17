cf0151cbd565af3d5b79189fbd1d08524f91d0f2
Swaroop <141646466+Swaroop5222@users.noreply.github.com>
2023-09-17 15:54:59 +0530

    Add files via upload

---
+++ print a multiplication table.py
@@ -0,0 +1,8 @@
+number = int(input("Enter the number for its multiplication table: "))
+i=1
+#for i in range(1,11):
+    #print(f"{number}x{i}=" )
+    #i+=1
+while(i<11):
+    print(f"{number}x{i}={number*i}" )
+    i+=1
